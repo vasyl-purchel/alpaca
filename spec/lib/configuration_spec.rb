13f1857388eccc39e7dedc4a7eaa3058b1494252
Vasyl Purchel <vasyl.purchel@gmail.com>
2015-03-15 18:39:09 +0000

    Fixed offences and added build status to README

--- spec/lib/configuration_spec.rb
+++ spec/lib/configuration_spec.rb
@@ -2,9 +2,9 @@ require 'alpaca/entities/config'
 require 'alpaca/factories/config_factory'
 
 describe Alpaca::Config do
-  # todo: create specs for config class
+  # TODO: create specs for config class
 end
 
 describe Alpaca::ConfigFactory do
-  # todo: create specs for config factory
+  # TODO: create specs for config factory
 end
